9304987369a433914b154f1c31b7ae3e37d7f81f
reinnillo <todev30@outlook.com>
2024-06-25 23:56:50 -0500

    - agrege una animacion en las imagenes de la galeria al momento de hacer scroll.

--- js/filterPhoto.js
+++ js/filterPhoto.js
@@ -1,35 +1,34 @@
+import { isElementMiddleViewport } from "./verifyElementInViewport.js";
+
 document.addEventListener('DOMContentLoaded', () => {
-    
+
     // Obtenemos los botones de los filtros
     const allBtn = document.querySelector('.all');
     const weddingBtn = document.querySelector('.wedding');
     const boudoirBtn = document.querySelector('.boudoir');
     const petsBtn = document.querySelector('.pets');
     const pregnantBtn = document.querySelector('.pregnant');
+    const photo = document.querySelector('.photo');
     const gallery = document.querySelector('.portfolio-gallery').children;
     const params = new URLSearchParams(window.location.search);
 
     const filterPhoto = filter => {
-    
-        Array.from(gallery).forEach( photo => {
-    
+        Array.from(gallery).forEach(photo => {
             const filters = photo.getAttribute('data-filter').split(' ');
-    
-            if ( filters.includes(filter)) {
+            if (filters.includes(filter)) {
                 photo.style.display = 'block';
             } else {
                 photo.style.display = 'none';
             }
         });
-    
     };
 
     const changeBackgroundFilterBtn = (btn1, btn2, btn3, btn4, btn5) => {
-        if(btn1.classList.contains('active')) {
+        if (btn1.classList.contains('active')) {
             btn1.classList.remove('active');
         } else {
             btn1.classList.add('active');
-            
+
             // remover la clase 'active' a los demas botones
             btn2.classList.remove('active');
             btn3.classList.remove('active');
@@ -63,4 +62,22 @@ document.addEventListener('DOMContentLoaded', () => {
         filterPhoto('pregnant');
         changeBackgroundFilterBtn(pregnantBtn, allBtn, weddingBtn, boudoirBtn, petsBtn);
     })
+    
+    // animar las fotos cuando se hace scoll
+    const animateViewImg = () => { 
+        Array.from(gallery).forEach(photo => {
+            let img = photo.firstElementChild
+            
+            if (isElementMiddleViewport(img)) {
+                img.classList.add('view-img');
+                photo.style.boxShadow = '0 0 10px rgba(81, 203, 238, 1)';
+            } else {
+                img.classList.remove('view-img');
+                photo.style.boxShadow = 'none';
+            }
+        });
+        
+        requestAnimationFrame(animateViewImg);
+    }
+    animateViewImg()
 })
--- js/verifyElementInViewport.js
+++ js/verifyElementInViewport.js
@@ -4,19 +4,25 @@
  * @param {HTMLElement} element - El elemento HTML a verificar.
  * @returns {boolean} - Retorna true si el elemento está dentro del viewport, de lo contrario false.
  *
- * La función obtiene las dimensiones y posición del elemento en el viewport
- * usando `getBoundingClientRect()` y evalúa si todas las caras del elemento
- * (superior, izquierdo, inferior y derecho) están dentro de los límites del viewport
- * (ventana gráfica visible del navegador).
+ * La función obtiene las dimensiones del viewport y posición del elemento en el viewport
+ * usando `getBoundingClientRect()` y evalúa si el top y bottom del elemento
+ * (superior e inferior) están dentro de los límites del medio inferior del viewport
+ * (la mitad inferior de la ventana gráfica visible del navegador).
  */
-export function isElementInViewport(element) {
-    // Obtenemos las dimensiones y posición del elemento
-    let rect = element.getBoundingClientRect();
-    // Evaluamos si todas las caras del elemento están dentro del viewport
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+
+export function isElementMiddleViewport(element) {
+    const midViewPort = window.innerHeight / 2;
+    let topElement = element.getBoundingClientRect().top;
+    let bottomElement = element.getBoundingClientRect().bottom;
+    let heightElement = element.getBoundingClientRect().height / 5;
+
+    if (
+        topElement < 0 && bottomElement < (midViewPort - heightElement) || 
+        topElement > midViewPort) {
+
+        return false;
+    }
+    else {
+        return true;
+    }
 }

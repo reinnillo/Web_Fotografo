2ef304526f044fd34662ecab9697b8966447d835
reinnillo <todev30@outlook.com>
2024-06-28 19:44:28 -0500

    - cambie completamente la carpeta de las imagenes, e hice todos los ajustes en los archivos que las usanban para colocar las nuevas rutas. - modifique el script para mostrar las imagenes. - modifique la seccion de servicios para agregar una imagen en la ultima card de la red social de la propietaria de la web 'ipoteticamente existe'

--- js/presentation.js
+++ js/presentation.js
@@ -1,32 +1,33 @@
 // Obtener las variables.
 const presentation = document.querySelector('.presentation');
 const imgPortada = document.querySelector('#img-portada');
-
+let intervalActual = 0;
 const imgForMobile = [
-    '../assets/img/boudoir-photos/background-boudoir/sentada-sombra.jpg',
-    '../assets/img/boudoir-photos/modelo-1/boudoir-photo-12.jpg',
-    '../assets/img/boudoir-photos/background-boudoir/espalda-semiDes-bn.jpg',
-    '../assets/img/boudoir-photos/background-boudoir/atada-bn.jpg',
-    '../assets/img/boudoir-photos/modelo-1/boudoir-photo-11.jpg',
+    '../assets/img/boudoir/bo-1.jpg',
+    '../assets/img/bg-mobile/bg-mo-1.png',
+    '../assets/img/bg-mobile/bg-mo-2.jpg',
+    '../assets/img/boudoir/bo-2.jpg',
+    '../assets/img/bg-mobile/bg-mo-3.jpg',
+    '../assets/img/boudoir/bo-3.jpg',
+    '../assets/img/bg-mobile/profile.jpg',
 ];
 const imgList = [
-    '../assets/img/background/presentation/presentation-1.jpg',
-    '../assets/img/background/presentation/presentation-2.jpg',
-    '../assets/img/background/presentation/presentation-3.jpg',
-    '../assets/img/background/presentation/presentation-4.jpg',
-    '../assets/img/background/presentation/presentation-5.jpg',
-    '../assets/img/background/presentation/presentation-6.jpg',
+    '../assets/img/bg-desktop/bg-pc-1.jpg',
+    '../assets/img/bg-desktop/bg-pc-2.jpg',
+    '../assets/img/bg-desktop/bg-pc-3.jpg',
+    '../assets/img/bg-desktop/bg-pc-4.jpg',
+    '../assets/img/bg-desktop/bg-pc-5.jpg',
+    '../assets/img/bg-desktop/bg-pc-6.jpg',
 ];
 
 // Funcion para animar la imagen de portada del home
 function animatePortada(array, element, interval) {
     let i = 0;
-    setInterval(() => {
+    intervalActual = setInterval(() => {
         element.src = `${array[i]}`;
         i = (i + 1) % array.length;
     }, interval);
 }
-
 
 // Funcion para ajustar el tamano del elemento home igual al del dispositivo.
 function autoSize(element) {
@@ -40,26 +41,23 @@ window.onload = function () {
     autoSize(presentation);
     // mobile
     if (window.innerWidth <= 768) {
-        imgPortada.src = imgForMobile[0];
-        animatePortada(imgForMobile, imgPortada, 15000);
-        
+        imgPortada.src = imgForMobile[5];
+
     } else {
         // desktop
-        imgPortada.src = imgList[0];
+        imgPortada.src = imgList[5];
         animatePortada(imgList, imgPortada, 15000);
     }
 }
 window.onresize = function () {
     autoSize(presentation);
+    clearInterval(intervalActual);
     // mobile
     if (window.innerWidth <= 768) {
-        imgPortada.src = imgForMobile[0];
-        animatePortada(imgForMobile, imgPortada, 15000);
-        
+        imgPortada.src = imgForMobile[5];
+
     } else {
         // desktop
-        imgPortada.src = imgList[0];
-        animatePortada(imgList, imgPortada, 15000);
+        imgPortada.src = imgList[5];
     }
 }
-

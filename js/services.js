bb064e93ccd6c24e3284d309855cb0cbfab92df6
reinnillo <todev30@outlook.com>
2024-06-30 00:09:33 -0500

    - Recostruccion del la seccion de servicios.

--- js/services.js
+++ js/services.js
@@ -1,47 +1,48 @@
 
-// lista de div de "items services" con su h3 y su parrafo.
-const servicesItemsH3_List = document.querySelectorAll('.item-h3');
+// lista de div de "items services" con su parrafo.
 const servicesItemsP_List = document.querySelectorAll('.item-p');
-// lista de imagenes de cada tipo
-const servicesImgList = document.querySelectorAll('.img-box');
 
 // funcion para animar la muestra de los servicios y sus imagenes.
-const changeIgmAndTextFromServices = (item) => {
-    const we = 0;
-    const bo = 1;
-    const pe = 2;
-    const pr = 3;
-    const primaryOpacity = 5;
-    const secondaryOpacity = 0;
-    
-    servicesItemsH3_List[item].firstElementChild.addEventListener('mouseover', (event) => {
-        switch (event.target.innerText) {
-            case servicesItemsH3_List[item].firstElementChild.innerText:
+let interval = 0;
+const changeTextFromServices = (array, int) => {
+    const onOpacity = 5;
+    const offOpacity = 0;
+    let count = 0;
 
-                // valores por defecto de Bodas
-                servicesItemsP_List[we].style.opacity = secondaryOpacity;
-                servicesImgList[we].style.opacity = secondaryOpacity;
-
-                // valores por defecto de Boudoir
-                servicesItemsP_List[bo].style.opacity = secondaryOpacity;
-                servicesImgList[bo].style.opacity = secondaryOpacity;
-
-                // valores por defecto de Pet
-                servicesItemsP_List[pe].style.opacity = secondaryOpacity;
-                servicesImgList[pe].style.opacity = secondaryOpacity;
-
-                // valores por defecto de Pregnant
-                servicesItemsP_List[pr].style.opacity = secondaryOpacity;
-                servicesImgList[pr].style.opacity = secondaryOpacity;
-
-                // valores para la animacion.
-                servicesItemsP_List[item].style.opacity = primaryOpacity;
-                servicesImgList[item].style.opacity = primaryOpacity;
+    interval = setInterval(() => {
+        if (count === 4) { 
+            count = 0;
+        };
+        switch (count) {
+            case 0:
+                array[0].style.opacity = onOpacity;
+                array[1].style.opacity = offOpacity;
+                array[2].style.opacity = offOpacity;
+                array[3].style.opacity = offOpacity;
                 break;
+            case 1:
+                array[0].style.opacity = offOpacity;
+                array[1].style.opacity = onOpacity;
+                array[2].style.opacity = offOpacity;
+                array[3].style.opacity = offOpacity;
+                break;
+            case 2:
+                array[0].style.opacity = offOpacity;
+                array[1].style.opacity = offOpacity;
+                array[2].style.opacity = onOpacity;
+                array[3].style.opacity = offOpacity;
+                break;
+            case 3:
+                array[0].style.opacity = offOpacity;
+                array[1].style.opacity = offOpacity;
+                array[2].style.opacity = offOpacity;
+                array[3].style.opacity = onOpacity;
+                break;
+            default:
+                clearInterval(interval);
         }
-    })
+        count += 1;
+        console.log(interval);
+    }, int)
 }
-changeIgmAndTextFromServices(0);
-changeIgmAndTextFromServices(1);
-changeIgmAndTextFromServices(2);
-changeIgmAndTextFromServices(3);
+changeTextFromServices(servicesItemsP_List, 10000);
